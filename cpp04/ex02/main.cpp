ad516e351a32d2986a57eb1aa898b30e473346a8
Pierre-Loup Scala <pscala@f2r10s9.clusters.42paris.fr>
2025-07-04 22:52:06 +0200

    jojolion

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pscala <pscala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:53:18 by pscala            #+#    #+#             */
-/*   Updated: 2025/03/13 10:48:08 by pscala           ###   ########.fr       */
+/*   Updated: 2025/05/29 07:37:16 by pscala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ int	main(void)
 {
 	const Animal 	*tab[10];
 	const Animal	*j = new Dog();
+	const Animal	*p = new Dog();
 	const Animal	*i = new Cat();
 
+	p = j;
+	Dog dog;
+
+	Dog tmp = dog;
+	std::cout << "\n" << BOLD << "p: this is a " << BLUE << p->getType() << " " << RESET << std::endl;
 	std::cout << "\n" << BOLD << "j: this is a " << BLUE << j->getType() << " " << RESET << std::endl;
 	std::cout << BOLD << "i: this is a " << YELLOW << i->getType() << " " << RESET << std::endl;
 
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pscala <pscala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:44:33 by pscala            #+#    #+#             */
-/*   Updated: 2025/04/12 19:27:01 by pscala           ###   ########.fr       */
+/*   Updated: 2025/06/08 14:43:02 by pscala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int	main(void)
 {
 	try
 	{
+		std::cout << BOLD << GREEN << "test 1:" << std::endl;
 		Bureaucrat g("Jojo1", 8);
 		std::cout << BOLD << BLUE << g << RESET << std::endl;
-		Bureaucrat h("Jojo2", 151);
+		Bureaucrat h("Jojo2", 150);
 		std::cout << BOLD << BLUE << h << RESET << std::endl;
 	}
 	catch (const std::exception &e)
@@ -27,9 +28,15 @@ int	main(void)
 	}
 	try
 	{
+		std::cout << BOLD << GREEN << "test 2:" << std::endl;
+
 		Bureaucrat a("Jojo3", 8);
+		std::cout << BOLD << BLUE << a << RESET << std::endl;
 		Bureaucrat b("Jojo4", 150);
+		std::cout << BOLD << BLUE << b << RESET << std::endl;
+
 		a.decrement();
+		std::cout << BOLD << BLUE << a << RESET << std::endl;
 		b.decrement();
 	}
 	catch (const std::exception &e)
@@ -38,6 +45,8 @@ int	main(void)
 	}
 	try
 	{
+		std::cout << BOLD << GREEN << "test 3:" << std::endl;
+
 		Bureaucrat g("Jojo5", 0);
 		Bureaucrat h("Jojo6", 150);
 	}
@@ -47,6 +56,8 @@ int	main(void)
 	}
 	try
 	{
+		std::cout << BOLD << GREEN << "test 4:" << std::endl;
+
 		Bureaucrat a("Jojo7", 1);
 		Bureaucrat b("Jojo8", 150);
 		a.increment();
